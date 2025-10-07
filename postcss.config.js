a86878772f6d2cdc3801d3aacf9ec7b9db5a2cad
VoltaiLTD <voltai.ltd@hotmail.com>
2025-10-07 02:23:48 +0100

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Keep only if you have nested CSS (like `&:hover` in plain CSS):
+    // 'postcss-nesting': {},
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};

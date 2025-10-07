24072f26a90bb43a86dabeb5c1ff114b7524045a
VoltaiLTD <voltai.ltd@hotmail.com>
2025-10-07 02:20:10 +0100

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // Keep only if you have nested CSS (like `&:hover` in plain CSS):
-    // 'postcss-nesting': {},
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-};
